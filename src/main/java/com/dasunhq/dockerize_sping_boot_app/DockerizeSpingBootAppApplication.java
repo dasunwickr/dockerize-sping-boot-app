656d578676b7c9e1f6e5c06450daf6cdf3b9b0fc
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-02-08 14:27:52 +0530

    feat: git init

---
+++ src/main/java/com/dasunhq/dockerize_sping_boot_app/DockerizeSpingBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.dasunhq.dockerize_sping_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeSpingBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeSpingBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasunhq/dockerize_sping_boot_app/DockerizeSpingBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasunhq.dockerize_sping_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizeSpingBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
